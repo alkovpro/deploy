a16c41c5a61bc750e25d0cbf211d7a4c4b7e5df3
Alexander Kovalev <alexander.kovalev@viacode.com>
2016-05-02 03:57:22 +0900

    code reformat

--- deploy.py
+++ deploy.py
@@ -1,17 +1,18 @@
-from flask import Flask, redirect, url_for
+from flask import Flask
 import os
 
-TIMEFILE='/home/www/deploy/lastdeploy.time'
-LOGFILE='/home/www/deploy/lastdeploy.log'
-STDFILE='/home/www/deploy/lastdeploy.std'
-ERRFILE='/home/www/deploy/lastdeploy.err'
-TRIGGER_PATH='/home/www/deploy/trigger.'
-PROCESS=TRIGGER_PATH+'process'
-TRIGGER=TRIGGER_PATH+'trigger'
-QUEUE  =TRIGGER_PATH+'queue'
-WAIT   =TRIGGER_PATH+'wait'
+TIMEFILE = '/home/www/deploy/lastdeploy.time'
+LOGFILE = '/home/www/deploy/lastdeploy.log'
+STDFILE = '/home/www/deploy/lastdeploy.std'
+ERRFILE = '/home/www/deploy/lastdeploy.err'
+TRIGGER_PATH = '/home/www/deploy/trigger.'
+PROCESS = TRIGGER_PATH + 'process'
+TRIGGER = TRIGGER_PATH + 'trigger'
+QUEUE = TRIGGER_PATH + 'queue'
+WAIT = TRIGGER_PATH + 'wait'
 
 application = Flask(__name__)
+
 
 @application.route('/')
 def index():
@@ -22,36 +23,40 @@ def index():
 def pushed():
     if not os.path.isfile(PROCESS):
         if os.path.isfile(WAIT):
-            os.rename(WAIT,TRIGGER)
+            os.rename(WAIT, TRIGGER)
     else:
-        os.rename(PROCESS,QUEUE)
+        os.rename(PROCESS, QUEUE)
 
     return 'done<br/><br/><a href="/last">Check last result</a>'
 
+
 @application.route('/last', methods=['GET', 'POST'])
 def lastdeploy():
-    timefile=''
-    logfile=''
-    stdfile=''
-    errfile=''
+    timefile = ''
+    logfile = ''
+    stdfile = ''
+    errfile = ''
     try:
-        timefile=file(TIMEFILE).read()
-    except:pass
+        timefile = file(TIMEFILE).read()
+    except:
+        pass
     try:
-        logfile=file(LOGFILE).read()
-    except:pass
+        logfile = file(LOGFILE).read()
+    except:
+        pass
     try:
-        stdfile=file(STDFILE).read()
-    except:pass
+        stdfile = file(STDFILE).read()
+    except:
+        pass
     try:
-        errfile=file(ERRFILE).read()
-    except:pass
-    if len(timefile)>0:
-        timefile=" (%s)" % timefile
-    res='<b>Log'+timefile+':</b><br/><pre>'+ logfile + \
-        '</pre><hr><b>Std out'+timefile+':</b><br/><pre>'+ stdfile + \
-        '</pre><hr><b>Std err'+timefile+':</b><br/><pre>'+ errfile + \
-        '</pre>'
+        errfile = file(ERRFILE).read()
+    except:
+        pass
+    if len(timefile) > 0:
+        timefile = " (%s)" % timefile
+    res = '<b>Log' + timefile + ':</b><br/><pre>' + logfile + \
+          '</pre><hr><b>Std out' + timefile + ':</b><br/><pre>' + stdfile + \
+          '</pre><hr><b>Std err' + timefile + ':</b><br/><pre>' + errfile + \
+          '</pre>'
 
     return res
-
--- run.py
+++ run.py
@@ -1,7 +1,4 @@
 from deploy import application
-#import os
-
-#ROOTDIR = os.path.abspath(os.path.dirname(__file__))
 
 if __name__ == "__main__":
     application.run()
